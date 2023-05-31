84ab959a5adf1b878a70a0412a5604f97bbb4787
llinjae <dldlswognqh@naver.com>
2023-05-31 18:03:35 +0900

    arrayType01

---
+++ Array_Types/01.ts
@@ -0,0 +1,13 @@
+// 대괄호 앞에 어떤타입인지 알려줘야함
+const activeUsers: string[] = [];
+
+activeUsers.push('Tony');
+
+// error
+activeUsers.push(12);
+
+const ageList: number[] = [45, 56, 13];
+ageList[0] = 99;
+
+// error
+ageList[0] = 'asd'
