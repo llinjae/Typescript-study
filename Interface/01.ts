5b196e70c106c71acc18ad009c602511f62a9cea
llinjae <dldlswognqh@naver.com>
2023-06-01 15:16:27 +0900

    interface-readonly

--- Interface/01.ts
+++ Interface/01.ts
@@ -11,3 +11,20 @@ interface Point {
 }
 
 const pt: Point = { x: 123, y: 1234 };
+
+interface Person {
+  readonly id: number;
+  first: string;
+  last: string;
+  nickname?: string;
+}
+
+const thomas: Person = {
+  first: 'Thomas',
+  last: 'Hardy',
+  nickname: 'Tom',
+  id: 21312,
+};
+
+thomas.first = 'kasd';
+thomas.id = 12312;
