b79a8ba2abad2776131044fa73cfcfa79163958f
llinjae <dldlswognqh@naver.com>
2023-06-01 13:52:54 +0900

    Tuple-02

--- Tuple/01.ts
+++ Tuple/01.ts
@@ -3,3 +3,13 @@
 // const color: number[] = [23, 45, 234, 234];
 const color: [number, number, number] = [255, 0, 45];
 
+type HTTPResponse = [number, string];
+
+const goodRes: HTTPResponse = [200, 'OK'];
+// goodRes[0] = '200';
+goodRes.push(123);
+
+const responses: HTTPResponse[] = [
+  [404, 'Not Found'],
+  [200, 'OK'],
+];
